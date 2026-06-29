78bb033adc57a2265f3bd7720bf0c6b0d19992e2
unknown <rohit.skstechsolution@gmail.com>
2026-06-29 21:07:23 +0530

    Deployed on Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <HelmetProvider>
         <App />
+        <SpeedInsights />
       </HelmetProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
