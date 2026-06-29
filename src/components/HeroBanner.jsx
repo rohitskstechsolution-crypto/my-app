dbbaf7db7edef60e867572d27a7d276ccc8a5bca
unknown <rohit.skstechsolution@gmail.com>
2026-06-29 21:28:02 +0530

    Update homepage

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -6,7 +6,7 @@ export default function HeroBanner() {
     <section className='relative  h-screen' style={{ backgroundImage: "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
         <div className='absolute inset-0 bg-black/30'></div>
         <div className='relative max-w-7xl text-center mx-auto flex flex-col justify-center items-center h-full'>
-            <h1 className='text-5xl font-bold text-white pb-5'>Welcome to My Blog</h1>
+            <h1 className='text-5xl font-bold text-white pb-5'>Welcome Rohit</h1>
             <p className='text-lg text-gray-300'>Discover the latest news and insights</p>
             <div>
                 <button className='bg-blue-600 text-white px-6 py-3 rounded-md mt-5 hover:bg-blue-700 transition duration-300'><Link to='/blog'>View Posts</Link></button>
